97e4c4d943ceff785047ebff2049e277b01e6733
takase1121 <20792268+takase1121@users.noreply.github.com>
2021-02-27 11:25:41 +0800

    refactor

--- init.lua
+++ init.lua
@@ -27,30 +27,32 @@ function NagView:get_title()
   return self.title
 end
 
+function NagView:each_option()
+  return coroutine.wrap(function()
+    for i = #self.options, 1, -1 do
+      coroutine.yield(i, self.options[i])
+    end
+  end)
+end
+
 function NagView:get_options_height()
   local max = 0
-  for _, text in ipairs(self.options) do
-    local lh = style.font:get_height(text)
+  for _, opt in ipairs(self.options) do
+    local lh = style.font:get_height(opt.text)
     if lh > max then max = lh end
   end
   return max
 end
 
 function NagView:get_line_height()
-  if core.active_view == self then
-    return 2 * BORDER_WIDTH + 2 * BORDER_PADDING + math.max(style.font:get_height(self.message), self:get_options_height()) + style.padding.y
-   else
-    return 0
-  end
-end
-
-function NagView:get_scrollable_size()
-  return 0
+  local maxlh = math.max(style.font:get_height(self.message), self:get_options_height())
+  return 2 * BORDER_WIDTH + 2 * BORDER_PADDING + maxlh + 2 * style.padding.y
 end
 
 function NagView:update()
   NagView.super.update(self)
-  local dest = self:get_line_height()
+
+  local dest = core.active_view == self and self:get_line_height() or 0
   self:move_towards(self.size, "y", dest)
 end
 
@@ -63,31 +65,31 @@ function NagView:draw_overlay()
   end)
 end
 
-function NagView:each_option()
+function NagView:each_visible_option()
   return coroutine.wrap(function()
     local halfh = math.floor(self.size.y / 2)
     local ox, oy = self:get_content_offset()
     ox = ox + self.size.x - style.padding.x
 
-    for i, v in ipairs(self.options) do
-      local lw, lh = style.font:get_width(v), style.font:get_height(v)
+    for i, opt in self:each_option() do
+      local lw, lh = opt.font:get_width(opt.text), opt.font:get_height(opt.text)
       local bw, bh = (lw + 2 * BORDER_WIDTH + 2 * BORDER_PADDING), (lh + 2 * BORDER_WIDTH + 2 * BORDER_PADDING)
       local halfbh = math.floor(bh / 2)
-      local box, boy = math.max(0, ox - bw), math.max(0, oy + halfh - halfbh)
+      local bx, by = math.max(0, ox - bw), math.max(0, oy + halfh - halfbh)
       local fw, fh = bw - 2 * BORDER_WIDTH, bh - 2 * BORDER_WIDTH
-      local fox, foy = box + BORDER_WIDTH, boy + BORDER_WIDTH
-      coroutine.yield(i, box, boy, bw, bh, fox, foy, fw, fh, v)
+      local fx, fy = bx + BORDER_WIDTH, by + BORDER_WIDTH
+      coroutine.yield(i, opt, bx,by,bw,bh, fx,fy,fw,fh)
       ox = ox - bw - style.padding.x
     end
   end)
 end
 
-function NagView:on_mouse_moved(x, y, ...)
-  NagView.super.on_mouse_moved(self, x, y, ...)
+function NagView:on_mouse_moved(mx, my, ...)
+  NagView.super.on_mouse_moved(self, mx, my, ...)
 
   local selected = false
-  for i, ox, oy, w, h in self:each_option() do
-    if x >= ox and y >= oy and x < ox + w and y < oy + h then
+  for i, _, x,y,w,h in self:each_visible_option() do
+    if mx >= x and my >= y and mx < x + w and my < y + h then
       self.selected = i
       selected = true
       break
@@ -105,7 +107,6 @@ function NagView:on_mouse_pressed(...)
 end
 
 function NagView:on_select(item)
-  print(item)
 end
 
 function NagView:draw()
@@ -119,17 +120,17 @@ function NagView:draw()
   common.draw_text(style.font, config.nagbar_text_color, self.message, "left", ox + style.padding.x, oy, self.size.x, self.size.y)
 
   -- draw buttons
-  for i, box, boy, bw, bh, fox, foy, fw, fh, text in self:each_option() do
+  for i, opt, bx,by,bw,bh, fx,fy,fw,fh in self:each_visible_option() do
     local fill = i == self.selected and config.nagbar_text_color or config.nagbar_color
     local text_color = i == self.selected and config.nagbar_color or config.nagbar_text_color
 
-    renderer.draw_rect(box, boy, bw, bh, config.nagbar_text_color)
+    renderer.draw_rect(bx,by,bw,bh, config.nagbar_text_color)
 
     if i ~= self.selected then
-      renderer.draw_rect(fox, foy, fw, fh, fill)
+      renderer.draw_rect(fx,fy,fw,fh, fill)
     end
 
-    common.draw_text(style.font, text_color, text, "center", fox, foy, fw, fh)
+    common.draw_text(opt.font, text_color, opt.text, "center", fx,fy,fw,fh)
   end
 end
 
